223d548bb0bfef64559e2c9deba778b7ee4e4484
Istabraq <angel.ofmoons@gmail.com>
2017-06-18 19:37:39 -0400

    Display calculation date

--- display_results.php
+++ display_results.php
@@ -69,6 +69,8 @@
 
         <label>Future Value:</label>
         <span><?php echo $future_value_f; ?></span><br>
+
+	<h4>This calculation was done on <?php echo date('m/d/Y'); ?>.</h4>
     </main>
 </body>
 </html>
